1f1babd02bf9870f0636a4991051f7beeb9e6a4c
Daniel Schmidt <sdanie04@students.uni-mainz.de>
2022-10-01 01:06:13 +0200

    Player, Monster and Camera initial setup

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+
+    // Called only once
+    void Awake() {
+
+    }
+
+    // Called once per Frame
+    void Update() {
+        
+    }
+
+    // Called a fixed amount of times per second
+    // Use for physics
+    void FixedUpdate() {
+
+    }
+
+}
