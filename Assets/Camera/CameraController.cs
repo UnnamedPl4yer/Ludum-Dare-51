1567e175a60ee5d735bb077704f845db80f254a1
Daniel Schmidt <sdanie04@students.uni-mainz.de>
2022-10-01 02:09:12 +0200

    Playercontroller und Kameracontroller
    # Playercontroller
    - Movement
    - Atem anhalten und wieder Luft holen
    -- Plus "husten" für später
    
    # Kameracontroller
    - Folgt Spieler (Lerp)

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform playerTransform;
+    public float cameraSpeed = 2.0f;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Vector3 position = transform.position;
+        position.x = Mathf.Lerp(transform.position.x, playerTransform.position.x, cameraSpeed * Time.deltaTime);
+        position.y = Mathf.Lerp(transform.position.y, playerTransform.position.y, cameraSpeed * Time.deltaTime);
+        transform.position = position;
+    }
+
+    IEnumerator LerpToPlayerLocation(float durationInSeconds) {
+        float time = 0;
+        Vector2 startPosition = transform.position;
+        while (time < durationInSeconds) {
+            transform.position = Vector2.Lerp(startPosition, playerTransform.position, time / durationInSeconds);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = playerTransform.position;
     }
 }
--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,22 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1.0f;
+    public float moveSpeed = 300.0f;
 
-    // Called only once
-    void Awake() {
+    // Stealth - Stop breathing
+    public int maxAir = 100;
+    public int airDecreaseRate = 10; // per second
+    public int airIncreaseRate = 5; // per second
+    public int chokingThreshold = 20;
+    public float airMoveMultiplier = 1.2f; // x increase while moving
+    [SerializeField] private float currentAir = 100;
+    [SerializeField] private bool isChoking = false;
+    
+    private Rigidbody2D rb;
 
+    // Called only once after GameObject initialization
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Called once per Frame
     void Update() {
-        
+        if (Input.GetKey("e") && !isChoking) {
+            DecreaseAir();
+        } else {
+            IncreaseAir();
+        }
     }
 
-    // Called a fixed amount of times per second
-    // Use for physics
+    // Called a fixed amount of times per second - Use for physics!
     void FixedUpdate() {
-
+        Vector2 moveDirection = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")
+        ).normalized;
+        Vector2 force = moveDirection * moveSpeed * Time.deltaTime;
+        rb.AddForce(force);
     }
 
+    // Stealth - stop breathing
+    private void DecreaseAir() {
+        float moveMultiplier = Mathf.Abs(rb.velocity.x) >= 0.1f || Mathf.Abs(rb.velocity.y) >= 0.1f ? airMoveMultiplier : 1;
+        Debug.Log(moveMultiplier);
+        currentAir -= (airDecreaseRate * moveMultiplier * Time.deltaTime);
+        if (currentAir <= 0) {
+            currentAir = 0;
+            isChoking = true;
+        }
+    }
+
+    private void IncreaseAir() {
+        currentAir += (airIncreaseRate * Time.deltaTime);
+        if (currentAir >= maxAir) {
+            currentAir = maxAir;
+        }
+        if (currentAir >= chokingThreshold) {
+            isChoking = false;
+        }
+    }
+    // End Stealth - stop breathing
 }
